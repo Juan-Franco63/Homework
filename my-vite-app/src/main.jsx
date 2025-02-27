2688bd58385e6b6154648747eb3d3c8ef8317b05
Juan Pablo Franco Herrera <juan_pablo.franco@uao.edu.co>
2025-02-26 19:23:51 -0500

    Challenge-04

--- my-vite-app/src/main.jsx
+++ my-vite-app/src/main.jsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import FirstApp from './FirstApp';
-import './index.css'; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ComponentApp } from "./ComponentApp";
+import "./index.css";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <FirstApp />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <ComponentApp />
+    </React.StrictMode>
 );
